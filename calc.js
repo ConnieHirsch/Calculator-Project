068fdad56743279c6f5b7fe8048eee47200e5a39
Connie Hirsch <connie.hirsch@gmail.com>
2018-11-08 16:15:25 -0500

    Revert "I've screwed up badly -- back to previous"
    This reverts commit 3a09f959726ad21146091a0993f074939bb1b95a.

--- calc.js
+++ calc.js
@@ -8,14 +8,11 @@ var MyAddend = 0;
 var operand;
 // holds keypress value from keyboard
 var eKeypress;
-// running total flag
-var runningTotalFlag = 0;
-var runningSum = 0;
 
 /*
- ** If the user clicks or taps the entry buttons on the screen, send the
- ** value directly to runTheCalc()
- */
+** If the user clicks or taps the entry buttons on the screen, send the
+** value directly to runTheCalc()
+*/
 $('button').click(function() {
     var myEntry = $(this).text();
     runTheCalc(myEntry);
@@ -23,41 +20,26 @@ $('button').click(function() {
 
 
 /*
- ** central calculation engine
- ** number and decimal values are parsed into operation values
- ** C means clear summation value
- ** +,-,x,/ are operands
- ** = signals end of adding operand, summing up operation, and commences
- **      actual calculation.  This runs a parseFloat to get decimal result
- ** default: somehow something went wrong.
- */
+** central calculation engine
+** number and decimal values are parsed into operation values
+** C means clear summation value
+** +,-,x,/ are operands
+** = signals end of adding operand, summing up operation, and commences
+**      actual calculation.  This runs a parseFloat to get decimal result
+** default: somehow something went wrong.
+*/
 function runTheCalc(myEntry) {
     if (myEntry == "C") {
         sum = 0;
         addend = 0;
-        runningTotalFlag = 0;
         $('#summation').text("0");
         $('#addend').text("");
     } else if (myEntry == "+" || myEntry == "-" || myEntry == "x" || myEntry == "/") {
+        MyAddend = sum;
         operand = myEntry;
-        if (runningTotalFlag == 1) {
-            // first calculate
-            console.log("Running total is being calculated!");
-            sum = parseFloat($('#summation').text());
-            console.log("running addend: " + MyAddend);
-            console.log("running sum for operation: " + sum);
-            sum = runningSum;
-            $('#summation').text("0");
-            $('#addend').text("Running total:" + runningSum);
-            //we should be set up for next entry, a number to continue the running total.
-        } else {
-            MyAddend = sum;
-            $('#addend').text(myEntry + " " + MyAddend);
-            runningSum = Calculate(operand, MyAddend, sum);
-            sum = 0;
-            runningTotalFlag = 1;
-            $('#summation').text("0");
-        }
+        $('#addend').text(myEntry + " " + MyAddend);
+        sum = 0;
+        $('#summation').text("0");
     } else if (myEntry == "=") {
         // show the first half of the calculation -- I can forget the number!
         console.log("addend: " + MyAddend);
@@ -66,25 +48,24 @@ function runTheCalc(myEntry) {
         var product = MyAddend + operand + sum;
         $('#addend').text(product);
         // now we actually calculate and handle result.
-        sum = Calculate(operand, MyAddend, sum);
+        sum = Calculate(operand);
         // onto the closing act!
         floatedString = parseFloat(sum);
         // Sooner or later, someone will try dividing by 0
-        if (floatedString.toString() == "Infinity") {
-            alert("Are you crazy?  \nYou're not allowed to divide by ZERO! \n You could have blown up the computer!");
-        }
+                if (floatedString.toString() == "Infinity") {
+                    alert("Are you crazy?  \nYou're not allowed to divide by ZERO! \n You could have blown up the computer!");
+                }
         $('#summation').text(floatedString.toString());
         $('.ticker').prepend(product + " = " + floatedString.toString() + "<br/>").show();
         sum = 0;
         addend = 0;
-        runningTotalFlag = 0;
 
     } else {
         // We are pressing a number key
         // Put each number in the Summation box until we hit an Op Key.
         // if there is no value, put a 0 in the summation box, but
         // when we start a new calculation, no leading zero.
-        sum === 0 ? sum = myEntry : sum = sum + myEntry;
+        sum === 0 ? sum = myEntry: sum = sum + myEntry;
         $('#summation').text(sum);
     }
 
@@ -93,45 +74,45 @@ function runTheCalc(myEntry) {
 // fixed so whole number returns get no decimal,
 // decimal returns are rounded to 3
 function handleDecimals(value) {
-    if (value % 1 === 0) {
-        console.log(value + ' is a whole number');
-        return value;
-    } else {
-        console.log(value + ' is not a whole number');
-        value = value.toFixed(3);
-        return value;
-    }
+  if (value % 1 === 0) {
+    console.log(value + ' is a whole number');
+    return value;
+  } else {
+    console.log(value + ' is not a whole number');
+    value = value.toFixed(3);
+    return value;
+  }
 }
 
 // Calculate!
-function Calculate(operand, MyAddend, sum) {
-    switch (operand) {
-        case "+":
-            sum = handleDecimals(parseFloat(MyAddend) + parseFloat(sum));
-            break;
-        case "-":
-            sum = handleDecimals(parseFloat(MyAddend) - parseFloat(sum));
-            break;
-        case "x":
-            sum = handleDecimals(parseFloat(MyAddend) * parseFloat(sum));
-            break;
-        case "/":
-            sum = handleDecimals(parseFloat(MyAddend) / parseFloat(sum));
-            break;
-        default:
-            console.log("Problem with myEntry: " + myEntry);
-    }
-    return sum;
+function Calculate(operand){
+        switch (operand) {
+            case "+":
+                sum = handleDecimals(parseFloat(MyAddend) + parseFloat(sum));
+                break;
+            case "-":
+                sum = handleDecimals(parseFloat(MyAddend) - parseFloat(sum));
+                break;
+            case "x":
+                sum = handleDecimals(parseFloat(MyAddend) * parseFloat(sum));
+                break;
+            case "/":
+                sum = handleDecimals(parseFloat(MyAddend) / parseFloat(sum));
+                break;
+            default:
+                console.log("Problem with myEntry: " + myEntry);
+        }
+        return sum;
 }
 
 // Check for a keypress -- if we want to run this like a calculator
 //  on a desktop, must be able to use number pad.
 //  If the keycode is matched to the e.which, send the value on to runtheCalc()
 //  NOTE: all other keys only go to console.
-$(function() {
-    $(document).on('keypress', function(e) {
+$(function(){
+    $(document).on('keypress', function(e){
         console.log(e.which);
-        switch (e.which) {
+        switch(e.which){
             case 48:
                 runTheCalc("0");
                 break;
